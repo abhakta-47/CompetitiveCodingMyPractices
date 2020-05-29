c37c03a1f809c35d88406212f8892d294752dcf8
Abhakta19 <www.arnab4@gmail.com>
2020-05-29 19:07:51 +0530

    added gitignore

--- codechef/maylaunchtime/ctest.cpp
+++ codechef/maylaunchtime/ctest.cpp
@@ -4,125 +4,177 @@ using namespace std;
 
 #define ll long long
 
-//ll l_l, l_r, l_f, x, y, l, r, p;
-ll x, y, l, r;
-set<ll> bx, by, bl, br, bans;
-
-set<ll> decToBinary(ll n)
+ll x, y, l, r, l_r2;
+ll l_r;
+vector<ll> br, bl, sum1, sum2, v;
+vector<ll> decToBinary(ll n)
 {
-    set<ll> t;
+    vector<ll> t;
     t.clear();
     int i = 0;
+    if (n == 0)
+        t.push_back(0);
     while (n > 0)
     {
-        if (n % 2 == 1) //{
-            t.insert(i);
-        //cout<<i<<" ";}
+        t.push_back(n % 2);
         n = n / 2;
         i++;
     }
     return t;
 }
 
-int func()
+ll func4(ll t)
 {
-    cout << "in func " << x << y << l << r << " ";
-    bx.clear();
-    by.clear();
-    bl.clear();
-    br.clear();
-    bans.clear();
-    //ll x, y, l, r;
-    ll ans = 0;
-    //cin >> x >> y >> l >> r;
-    if (x * y == 0)
-        ans = l;
-    else if (l == r)
-        ans = l;
+    return (x & t) * (y & t);
+}
+
+ll max2(ll t1, ll t2)
+{
+    if (func4(t1) > func4(t2))
+        return t1;
+    else if (func4(t2) > func4(t1))
+        return t2;
     else
+        return min(t1, t2);
+}
+
+vector<ll> func2(ll i, bool sg, bool gs)
+{
+    ll t, bit_l, bit_r, bit_f;
+    ll x = i - 1;
+    vector<ll> funcOut;
+    funcOut.resize(i);
+    while (--i >= 0)
     {
-        // always x > y
-        if (x < y)
+        bit_l = bl[i];
+        bit_f = v[i];
+        bit_r = br[i];
+
+        // sg smaller than greater && gs greater than smaller
+        if (sg && gs)
+            t = bit_f;
+        else if (~sg && gs)
         {
-            x = x + y;
-            y = x - y;
-            x = x - y;
+            if (bit_r == 0 && bit_f == 1)
+                t = 0;
+            else
+                t = bit_f;
+            if (t != bit_r)
+                sg = true;
         }
-
-        // binarise
-        bx = decToBinary(x);
-        by = decToBinary(y);
-        bl = decToBinary(l);
-        br = decToBinary(r);
-
-        // main Calculations
-
-        ll xbit, ybit, lbit, rbit;
-        bool flag = false;
-        for (ll i = *(--bl.end()); i >= 0; --i)
+        else if (sg && ~gs)
         {
-            rbit = (br.find(i) == br.end()) ? 0 : 1;
-            lbit = (bl.find(i) == bl.end()) ? 0 : 1;
-            xbit = (bx.find(i) == bx.end()) ? 0 : 1;
-            ybit = (by.find(i) == by.end()) ? 0 : 1;
-
-            if (flag)
-            { // if ans is already less than r
-                if (xbit == 1 || ybit == 1)
-                    bans.insert(i);
-            }
+            if (bit_l == 1 && bit_f == 0)
+                t = 1;
+            else
+                t = bit_f;
+            if (t != bit_l)
+                gs = true;
+        }
+        else
+        {
+            if (bit_l == bit_r)
+                t = bit_l;
             else
             {
-                if (rbit == 1)
-                {
-                    if (xbit == 1 && ybit == 1)
-                        bans.insert(i);
-                    else if (xbit == 1)
-                    {
-                        ll t1 = *(--bx.lower_bound(i)) + *(--by.lower_bound(i));
-                        ll t2 = *(--br.lower_bound(i));
-                        t2 = *(--by.upper_bound(t2)) + i;
-                        if (t2 > t1)
-                            bans.insert(i);
-                    }
-                    else if (ybit == 1)
-                    {
-                        ll t1 = *(--bx.lower_bound(i)) + *(--by.lower_bound(i));
-                        ll t2 = *(--br.lower_bound(i));
-                        t2 = *(--bx.upper_bound(t2)) + i;
-                        if (t2 > t1)
-                            bans.insert(i);
-                    }
-                }
+                t = bit_f;
+                if (t == bit_r)
+                    gs = true;
+                if (t == bit_l)
+                    sg = true;
+                l_r2 = i;
             }
-
-            // if ans is already less than r
-            if (rbit == 1 && bans.find(i) == bans.end())
-                flag = true;
         }
-
-        set<ll>::iterator itr;
-
-        for (itr = bans.begin(); itr != bans.end(); itr++)
-            ans += pow(2, *itr);
+        //cout << i << " " << bit_r << " " << bit_f << " " << bit_l << " " << t << " " << gs << sg << " ";
+        funcOut[i] = t; //(t * pow(2, i) + funcOut[funcOut.size() - 1]);
+        //cout << funcOut[i] << "\n ";
     }
 
+    return funcOut;
+}
+
+ll func()
+{
+    br.clear();
+    bl.clear();
+    sum1.clear();
+    sum2.clear();
+    v.clear();
+    //input
+    //cin >> x >> y >> l >> r;
+    // x = 3;
+    // y = 5;
+    // l = 0;
+    // r = 9;
+    //cout << "*1\n";
+    br = decToBinary(r);
+    l_r = br.size();
+    bl = decToBinary(l);
+    v = decToBinary(x | y);
+    bl.resize(l_r, 0);
+    v.resize(l_r, 0);
+
+    ll i, ans;
+    sum2.resize(l_r);
+    for (i = 0; i < l_r; i++)
+    {
+        if (i == 0)
+            sum2[i] = v[i];
+        else
+            sum2[i] = sum2[i - 1] + v[i] * pow(2, i);
+    }
+    //cout << "*2\n";
+    sum1 = func2(l_r, false, false);
+    for (i = 1; i < l_r; i++)
+        sum1[i] = sum1[i - 1] + sum1[i] * pow(2, i);
+
+    // cout << "printing\n";
+    // for (i = 0; i < sum1.size(); i++)
+    //     cout << sum1[i] << " ";
+
+    ans = sum1[l_r - 1];
+    for (i = l_r2; i >= 1; i--)
+    {
+        if (v[i] == 1 && br[i] == 1)
+        {
+            ans = max2(ans, sum2[i - 1] + sum1[l_r - 1] - sum1[i]);
+        }
+    }
+    ans = max2(ans, l);
+    if (ans < l)
+    {
+        v.clear();
+        v = decToBinary(ans);
+        v.resize(l_r, 0);
+        for (i = l_r2; i >= 0; i--)
+        {
+            if (bl[i] == 1 && v[i] == 0)
+                ans += pow(2, i);
+            else if (bl[i] == 0 && v[i] == 1)
+                break;
+        }
+    }
     return ans;
 }
 
+// int main()
+// {
+//     func();
+// }
+
 int main()
 {
-    // ios_base::sync_with_stdio(false);
-    // cin.tie(NULL);
-    cout << "in";
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    //cout << "in";
     ll maxv, maxans, t, ans;
     l = 0;
     //cin >> x >> y >> l >> r;
     ll i;
     //cout << (x | y) << "\n";
-    for (x = 1; x < 50; x++)
+    for (x = 0; x < 50; x++)
     {
-        for (y = 1; y < 50; y++)
+        for (y = 0; y < 50; y++)
         {
             maxv = 0;
             maxans = 0;
@@ -150,20 +202,3 @@ int main()
     }
     return 0;
 }
-
-// int main()
-// {
-//     ios_base::sync_with_stdio(false);
-//     cin.tie(NULL);
-//     ll t, i;
-//     cin >> t;
-//     for (i = 0; i < t; i++)
-//     {
-//         //cout<<"Case #"<<i+1<<": ";
-//         bl.clear();
-//         br.clear();
-//         v.clear();
-//         func();
-//         cout << "\n";
-//     }
-// }
--- tst.cpp
+++ tst.cpp
@@ -3,64 +3,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void block(long int x)
-{
-    vector<long int> v;
-
-    // Converting the decimal number
-    // into its binary equivalent.
-    cout << "Blocks for " << x << " : ";
-    while (x > 0)
-    {
-        v.push_back(x % 2);
-        x = x / 2;
-    }
-
-    // Displaying the output when
-    // the bit is '1' in binary
-    // equivalent of number.
-    for (int i = 0; i < v.size(); i++)
-    {
-        if (v[i] == 1)
-        {
-            cout << i;
-            if (i != v.size() - 1)
-                cout << ", ";
-        }
-    }
-    cout << endl;
-}
-
-// Driver Function
 int main()
 {
-    // map<int, int> m;
-    // map<int, int>::iterator itm;
-
-    // m.insert(make_pair(1, 2));
-    // //cout << m.size() << " " << m.begin()->second << "   ";
-    // //m.erase(1);
-    // m.insert(make_pair(2, 3));
-    // //cout << m.size() << " " << m.begin()->second;
-
-    // for (itm = m.begin(); itm != m.end(); itm++)
-    //     cout << itm->second;
-
-    set<int> v1, v2;
-
-    v1.insert(1);
-    v1.insert(2);
-    v2.insert(1);
-    v2.insert(2);
-    //v1 = v2;
-    if (v1 == v2)
-        cout << "yes";
-    else
-        cout << "no";
-    // cout << upper_bound(v.begin(), v.end(), 4) - v.begin() << " ";
-    // cout << v.end() - lower_bound(v.begin(), v.end(), 2);
-
-    //cout << floor(5 * 1.0 / 3);
-
-    //cout<<s.lower_bound(2)-s.begin();
+    vector<int> v, v1;
+    for (int i = 0; i <= 5; i++)
+    {
+        v.push_back(i);
+    }
+    //v.resize(5, 1);
+    v.resize(7, 2);
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i];
+    }
 }
